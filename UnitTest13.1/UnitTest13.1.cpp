a4c0e777f0c6205c8c4c1ad38bbec1da6bbb3e11
Natalia-Duzhak <72571464+Natalia-Duzhak@users.noreply.github.com>
2021-01-13 12:24:05 +0300

    Add files via upload

---
+++ UnitTest13.1/UnitTest13.1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../13.1/var.cpp"
+#include "../13.1/var.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest131
+{
+	TEST_CLASS(UnitTest131)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			nsVar::n = 0;
+			Assert::AreEqual(nsVar::n, 0);
+		}
+	};
+}
